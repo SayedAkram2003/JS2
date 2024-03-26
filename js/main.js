dda69eb775fc3a01222513a98881d67ebdc4139a
SayedAkram2003 <163974799+SayedAkram2003@users.noreply.github.com>
2024-03-26 02:25:39 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+// var x =document.getElementById("demo");
+
+// x.innerHTML = "hello from js file";
+// x.style.color ="red";
+// x.style.textAlign = "center";
+
+
+// var x =10;
+// var y =20;
+
+// var z = x+y;
+
+var counter =0;
+
+function increment(){
+    counter++;
+    document.getElementById("lion").innerHTML = counter;
+}
+
+function decrement (){
+    counter--;
+    document.getElementById("lion").innerHTML = counter;
+}
